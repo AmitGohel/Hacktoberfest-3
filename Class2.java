d1935e0ef4d38765e4ceeae9820bc561004aaf85
kavinduL <33134093+kavinduL@users.noreply.github.com>
2017-10-27 01:05:06 +0530

    Update Class2.java
    Edited Class name

--- Class2.java
+++ Class2.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class NewClass {
+public class Class2 {
 
    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
